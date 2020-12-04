198dcddb2d2513d868a7fcbf01d84d7b489bb897
jordan gilbert <jordanag@umich.edu>
2020-12-03 23:38:44 -0500

    master commit

--- selectionSort.java
+++ selectionSort.java
@@ -1,2 +1,29 @@
 # Auto detect text files and perform LF normalization
 * text=auto
+
+public class SelectionSort {
+
+private int temp;
+
+
+public SelectionSort() {
+}
+
+
+public int[] basicSeclectionSort(int[] x) {
+
+for (int i=0; i<x.length;++i) {
+
+for(int j=i+1;j< x.length;j++) {
+
+if (x[i]> x[j]) {
+temp = x[i];
+x[i]=x[j];
+temp = x[j];
+}
+}
+
+}
+return x;
+}
+}
